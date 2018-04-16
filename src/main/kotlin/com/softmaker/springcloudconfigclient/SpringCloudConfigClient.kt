54ca8c2dfa5e9d01e3f0e8a152d889be1e1c2b14
Artur Gryglicki <artur.gryglicki@glantus.com>
2018-04-16 15:05:10 +0200

    Created minimalistic spring cloud config client

---
+++ src/main/kotlin/com/softmaker/springcloudconfigclient/SpringCloudConfigClient.kt
@@ -0,0 +1,23 @@
+package com.softmaker.springcloudconfigclient
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.context.config.annotation.RefreshScope
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class SpringCloudConfigClient
+
+fun main(args: Array<String>) {
+    runApplication<SpringCloudConfigClient>(*args)
+}
+
+@RefreshScope
+@RestController
+class GiveMeRefreshedProperty(@Value("\${refresh.me}") private val testProperty: String) {
+
+    @GetMapping("/testProperty")
+    fun getTestProperty() = testProperty
+}
---
+++ src/test/kotlin/com/softmaker/springcloudconfigclient/SpringCloudConfigClientTests.kt
@@ -0,0 +1,16 @@
+package com.softmaker.springcloudconfigclient
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringCloudConfigClientTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
